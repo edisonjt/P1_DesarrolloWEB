59455cff1c6fa996053359bfd21aa1d44d1ef73b
David <davidandres102@hotmail.com>
2022-12-02 04:27:49 -0500

    modelo sin terminar de la pelicula

---
+++ Model/PeliculaModel.php
@@ -0,0 +1,26 @@
+<?php
+include ("../Config/Database.php");
+class Pelicula{
+    private $id;
+    private $nombre;
+    private $genero;
+    private $estreno;
+    private $clasificacion;
+    private $sinopsis;
+    private $idioma;
+    private $tipo;
+
+
+    public function __construct()
+    {
+        
+    }
+
+  
+
+
+    
+}
+  
+
+?>
