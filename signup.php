bdeaaa155c112624d64a12a9c57b4257f27554d6
edisonjt <eddyt1999@gmail.com>
2022-12-02 00:47:03 -0500

    arreglo estilos sigin signup

--- signup.php
+++ signup.php
@@ -9,7 +9,7 @@
   <meta content="" name="description" />
 
   <!-- Favicon -->
-  <link href="../../assets/admin/img/favicon.ico" rel="icon" />
+  <link href="assets/admin/img/favicon.ico" rel="icon" />
 
   <!-- Google Web Fonts -->
   <link rel="preconnect" href="https://fonts.googleapis.com" />
@@ -21,14 +21,14 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet" />
 
   <!-- Libraries Stylesheet -->
-  <link href="../../assets/admin/lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet" />
-  <link href="../../assets/admin/lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css" rel="stylesheet" />
+  <link href="assets/admin/lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet" />
+  <link href="assets/admin/lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css" rel="stylesheet" />
 
   <!-- Customized Bootstrap Stylesheet -->
-  <link href="../../assets/admin/css/bootstrap.min.css" rel="stylesheet" />
+  <link href="assets/admin/css/bootstrap.min.css" rel="stylesheet" />
 
   <!-- Template Stylesheet -->
-  <link href="../../assets/admin/css/style.css" rel="stylesheet" />
+  <link href="assets/admin/css/style.css" rel="stylesheet" />
 </head>
 
 <body>
@@ -89,16 +89,16 @@
   <!-- JavaScript Libraries -->
   <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
-  <script src="../../assets/admin/lib/chart/chart.min.js"></script>
-  <script src="../../assets/admin/lib/easing/easing.min.js"></script>
-  <script src="../../assets/admin/lib/waypoints/waypoints.min.js"></script>
-  <script src="../../assets/admin/lib/owlcarousel/owl.carousel.min.js"></script>
-  <script src="../../assets/admin/lib/tempusdominus/js/moment.min.js"></script>
-  <script src="../../assets/admin/lib/tempusdominus/js/moment-timezone.min.js"></script>
-  <script src="../../assets/admin/lib/tempusdominus/js/tempusdominus-bootstrap-4.min.js"></script>
+  <script src="assets/admin/lib/chart/chart.min.js"></script>
+  <script src="assets/admin/lib/easing/easing.min.js"></script>
+  <script src="assets/admin/lib/waypoints/waypoints.min.js"></script>
+  <script src="assets/admin/lib/owlcarousel/owl.carousel.min.js"></script>
+  <script src="assets/admin/lib/tempusdominus/js/moment.min.js"></script>
+  <script src="assets/admin/lib/tempusdominus/js/moment-timezone.min.js"></script>
+  <script src="assets/admin/lib/tempusdominus/js/tempusdominus-bootstrap-4.min.js"></script>
 
   <!-- Template Javascript -->
-  <script src="../../assets/admin/js/main.js"></script>
+  <script src="assets/admin/js/main.js"></script>
 </body>
 
 </html>
